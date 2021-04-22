45d9fb9d1c3f214b0a96c48c3a18c0befaa951f0
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-04-22 23:36:39 +0530

    basic fronted setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductScreen from "../src/screens/ProductScreen";
 const App = () => {
   return (
-    <>
-      <h1>lets start this giant project</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-4">
+        {/* <Header /> */}
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+
+      <Footer />
+    </Router>
   );
 };
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product";
+const HomeScreen = () => {
+  return (
+    <>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;

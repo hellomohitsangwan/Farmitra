a65f36c07a16cfe8f3a3c2d7fc6a28455e680a54
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-01 11:01:24 +0530

    added colorsjs package

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import colors from "colors";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -7,9 +7,9 @@ const connectDB = async () => {
       useCreateIndex: true,
       useFindAndModify: false,
     });
-    console.log(`mongo DB connected:${conn.connection.host}`);
+    console.log(`mongo DB connected:${conn.connection.host}`.cyan.underline);
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message.red.underline.bold);
     //exit tghe whole process with failure
     process.exit(1);
   }

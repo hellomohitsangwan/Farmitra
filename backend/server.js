8e510785531d039c532fe1ce8f08e942af8b74b2
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-03 18:03:19 +0530

    error middleware setup done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const app = express();
 connectDB();
@@ -10,13 +11,10 @@ app.get("/", (req, res) => {
   res.send("home route");
 });
 app.use("/api/products", productRoutes);
-// app.get("/api/products", (req, res) => {
-//   res.json(products);
-// });
-// app.get("/api/products/:id", (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id);
-//   res.json(product);
-// });
+//middleware routes
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {

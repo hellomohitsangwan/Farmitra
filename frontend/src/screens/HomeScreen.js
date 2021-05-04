335ba73e3c0e90d7e9bfc00d1cc72f15e4e84a4f
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-04 08:57:05 +0530

    loader spinner and alert message stup done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
-// import products from "../products";
 import Product from "../components/Product";
-
-// import axios from "axios";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+//redux files
 import { listProducts } from "../actions/productActions";
 import { useDispatch, useSelector } from "react-redux";
 const HomeScreen = () => {
@@ -13,14 +13,13 @@ const HomeScreen = () => {
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
-  // const products = [];
   return (
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading . . . .</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger" children={error} />
       ) : (
         <Row>
           {products.map((product) => (
@@ -30,13 +29,6 @@ const HomeScreen = () => {
           ))}
         </Row>
       )}
-      {/* <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row> */}
     </>
   );
 };

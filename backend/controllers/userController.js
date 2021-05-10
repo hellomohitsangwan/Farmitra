df86109375c04cbe2bee2b1376f28b339e43b5bb
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-10 15:17:19 +0530

    JWT generartion done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-
+import generateToken from "../utils/generateToken.js";
 // @desc  Auth. user
 // @route POST /api/users/auth
 // @access Public
@@ -13,7 +13,7 @@ export const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);

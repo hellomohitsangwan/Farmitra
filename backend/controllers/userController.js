97f75da8d026fe2e2676c175eeeeba641f7d7269
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-12 16:53:17 +0530

    create user functioality done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,35 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc  Register user
+// @route POST /api/users
+// @access Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
 
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user details");
+  }
+});
 
 // @desc  get user profile
 // @route POST /api/users/profile

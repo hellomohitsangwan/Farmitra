a2d99cf5927c8929d379d8680d87d4130737bc4d
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-12 16:31:03 +0530

    done with the middleware formation and integration in profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
+
 // @desc  Auth. user
 // @route POST /api/users/auth
 // @access Public
@@ -18,5 +19,26 @@ export const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401);
     throw new Error("invalid email or password");
+  }
+});
+
+
+
+// @desc  get user profile
+// @route POST /api/users/profile
+// @access Public
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
   }
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
-
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
+
 export default router;

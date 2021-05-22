23c3b88bae8f1f25877851735bb6a0f44f8a1873
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-21 20:00:00 -0600

    error handling updated

---
+++ backend/secrets/razorpay.js
@@ -0,0 +1,2 @@
+export const RAZORPAY_ID = "rzp_test_hfXMHEUFlz2YeJ";
+export const RAZORPAY_SECRET = "uE5M74Fjywn7nUTNs6CEbcYx";
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,15 +10,31 @@ import axios from "axios";
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("/api/products");
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    const res = await axios.get("/api/products");
+    if (res.status === 500) {
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload: "check your network connection",
+      });
+      return;
+    }
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: res.data });
+    console.log(res);
   } catch (err) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        err.response && err.response.data.message
+        err.response && err.response.status === 500
+          ? "check your network connection"
+          : err.response && err.response.status !== 500
           ? err.response.data.message
-          : err.message,
+          : "check your network connection",
+      // credits < 30 ? "freshman" : credits <= 59 ? "sophomore"
+      // err.response && err.response.data.message
+      //   ? err.response.data.message
+      //   : "check your network connection"
+      //   ? err.response.status === 500
+      //   : "check your network connection",
     });
   }
 };

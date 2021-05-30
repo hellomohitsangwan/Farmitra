83f43c19041f07e73fbb6e81acd32098a51164f3
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-30 16:59:28 +0530

    store updated

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -42,10 +42,10 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 };
 
-export const userDetailReducer = (state = {}, action) => {
+export const userDetailReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
-      return { loading: true, ...state };
+      return { ...state, loading: true };
     case USER_DETAILS_SUCCESS:
       return { loading: false, user: action.payload };
     case USER_DETAILS_FAIL:

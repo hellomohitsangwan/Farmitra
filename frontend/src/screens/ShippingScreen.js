ce681f34b0e55e8184f81de23fca72a0c2434254
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-05-31 23:05:51 +0530

    checkout component done and integrated

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,7 +3,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import { saveShippingAddress } from "../actions/cartActions";
-
+import CheckoutSteps from "../components/CheckoutSteps";
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
@@ -31,6 +31,7 @@ const ShippingScreen = ({ history }) => {
   }, [history, userInfo]);
   return (
     <FormContainer>
+      <CheckoutSteps step1 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">

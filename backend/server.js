7ff8a5430b5abe094b639c2912b75c5ad3babe7f
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-02 21:48:16 +0530

    done with backend orderControler reout

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 // import cookieVerifyRoute from "./routes/cookieVerifyRoute.js";
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 // app.use("/api", cookieVerifyRoute);
 //middleware routes
 app.use(notFound);

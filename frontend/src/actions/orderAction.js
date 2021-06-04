445c74453ab4899c8d75779dc37fa486e341f863
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-04 23:18:34 +0530

    getOrderDetailsction done

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_SUCCESS,
 } from "../constants/orderConstants";
 import axios from "axios";
 
@@ -27,6 +30,36 @@ export const createOrder = (order) => async (dispatch, getState) => {
   } catch (err) {
     dispatch({
       type: ORDER_CREATE_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+    console.log(err);
+  }
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST });
+    const {
+      userLogin: { userInfo }, //deconstructs the userLogin state to get the token
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(`api/orders/${id}`, config);
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
       payload:
         err.response && err.response.data.message
           ? err.response.data.message

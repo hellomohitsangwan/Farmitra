59777410583971c3e075d11b3a5e3ccc3a906f84
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-07 22:27:02 +0530

    route which chenges the order status is added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,7 +48,30 @@ export const getOrderById = asyncHandler(async (req, res) => {
   if (order) {
     res.json(order);
   } else {
-    res.status(201);
+    res.status(404);
+    throw new Error("order not found");
+  }
+});
+
+// @desc    update order status to paid
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
     throw new Error("order not found");
   }
 });

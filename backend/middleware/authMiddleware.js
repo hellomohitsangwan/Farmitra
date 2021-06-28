910e816e75c941ecba6d680bbf23b4133604a32d
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-28 11:49:57 +0530

    admin middleware added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -24,3 +24,12 @@ export const protect = asyncHandler(async (req, res, next) => {
     throw new Error("Not authorized , no token");
   }
 });
+
+const adminMiddleware = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};

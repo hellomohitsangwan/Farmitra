ce16fd54996549a66f4325dac3f0555464312371
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-07-07 00:35:21 +0530

    user delete controller added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,12 +6,14 @@ import {
   registerUser,
   updateUserProfile,
   getUsers,
+  deleteUSer,
 } from "../controllers/userController.js";
 import { adminMiddleware, protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser).get(protect, adminMiddleware, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
+router.route("/:id").delete(protect, adminMiddleware, getUserProfile);
 router
   .route("/profile")
   .get(protect, getUserProfile)

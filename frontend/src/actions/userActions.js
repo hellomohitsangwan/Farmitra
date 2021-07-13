1163827c806b5ef0e44e4817eb103371f484be13
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-07-14 01:39:50 +0530

    user delete action added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -20,6 +20,9 @@ import {
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
   USER_LIST_RESET,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -180,6 +183,36 @@ export const listUsers = () => async (dispatch, getState) => {
   } catch (err) {
     dispatch({
       type: USER_LIST_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+    console.log(err);
+  }
+};
+
+export const deleteUser = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_DELETE_REQUEST });
+    const {
+      userLogin: { userInfo }, //deconstructs the userLogin state to get the token
+    } = getState();
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.delete(`/api/users/${id}`, config);
+
+    dispatch({
+      type: USER_DELETE_SUCCESS,
+    });
+  } catch (err) {
+    dispatch({
+      type: USER_DELETE_FAIL,
       payload:
         err.response && err.response.data.message
           ? err.response.data.message

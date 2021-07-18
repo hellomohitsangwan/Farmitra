2cfa5d6bf42ae9905308ad381d0081ca03ac08dd
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-07-19 01:05:28 +0530

    product update action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -11,6 +11,7 @@ import {
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
+  PRODUCT_UPDATE_REQUEST,
 } from "../constants/productConstants";
 import axios from "axios";
 export const listProducts = () => async (dispatch) => {
@@ -92,6 +93,35 @@ export const createProduct = () => async (dispatch, getState) => {
       },
     };
     const { data } = await axios.post(`/api/products`, {}, config);
+    dispatch({ type: PRODUCT_CREATE_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_UPDATE_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.put(
+      `/api/products/${product._id}`,
+      {},
+      config
+    );
     dispatch({ type: PRODUCT_CREATE_SUCCESS, payload: data });
   } catch (err) {
     dispatch({

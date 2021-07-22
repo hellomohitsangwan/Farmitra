405fdfa0e736ef420f2cc7e2076be9d882af9781
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-07-22 13:13:14 +0530

    file upload integrated

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -20,7 +20,7 @@ const ProductEditScreen = ({ match, history }) => {
   const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
-  //   const [uploading, setUploading] = useState(false)
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -60,23 +60,23 @@ const ProductEditScreen = ({ match, history }) => {
   }, [dispatch, history, productId, product, successUpdate]);
 
   const uploadFileHandler = async (e) => {
-    // const file = e.target.files[0]
-    // const formData = new FormData()
-    // formData.append('image', file)
-    // setUploading(true)
-    // try {
-    //   const config = {
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data',
-    //     },
-    //   }
-    //   const { data } = await axios.post('/api/upload', formData, config)
-    //   setImage(data)
-    //   setUploading(false)
-    // } catch (error) {
-    //   console.error(error)
-    //   setUploading(false)
-    // }
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+      const { data } = await axios.post("/api/upload", formData, config);
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
   };
 
   const submitHandler = (e) => {
@@ -145,7 +145,7 @@ const ProductEditScreen = ({ match, history }) => {
                 custom
                 onChange={uploadFileHandler}
               ></Form.File>
-              {/* {uploading && <Loader />} */}
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId="brand">

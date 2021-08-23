fea9faac9341841e4fb9463c9fe94ae9b0bfb4bc
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-08-23 12:54:39 +0530

    heroku setup

--- backend/server.js
+++ backend/server.js
@@ -36,16 +36,23 @@ cloudinary.config({
 });
 // app.use(cookieParser());
 connectDB();
-app.get("/", (req, res) => {
-  res.send("home route");
-});
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
-
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("home route");
+  });
+}
 
 // app.get("/api/config/razorpay", protect, (req, res) => {
 //   res.send(RAZORPAY_ID);

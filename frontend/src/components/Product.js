5ac371f8d4a17d9729c18669abeaf4eef449781d
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-08-23 14:36:46 +0530

    currency symbol change

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -36,7 +36,7 @@ const Product = ({ product }) => {
       />
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">
-          <Link to={`/product/₹{product._id}`}>{product.name}</Link>
+          <Link to={`/product/${product._id}`}>{product.name}</Link>
         </h5>
         {/* <div className="ratings mt-auto">
           <div className="rating-outer">
@@ -47,7 +47,7 @@ const Product = ({ product }) => {
           </div>
           <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
         </div> */}
-        <p className="card-text">${product.price}</p>
+        <p className="card-text">₹{product.price}</p>
         <Link
           to={`/product/${product._id}`}
           id="view_btn"

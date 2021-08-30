b56528c81699b6788edd73e7a7f940bd90ebf1c1
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-08-31 00:42:39 +0530

    no products found screen solved

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -18,7 +18,16 @@ const HomeScreen = ({ match }) => {
   }, [dispatch, keyword]);
   return (
     <>
-      <h1>Latest Products</h1>
+      {products?.length !== 0 ? (
+        <h1>Latest Products</h1>
+      ) : (
+        <>
+          <h1 className="text-center , py-3">Sorry no results found !!</h1>
+          <p className="text-center">
+            please check your spelling or try searching for something else
+          </p>
+        </>
+      )}
       {loading ? (
         <Loader />
       ) : error ? (

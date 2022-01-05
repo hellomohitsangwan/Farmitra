4f389980a92ae08667887c8fd0e7010e663a9497
mohit kumar <hellomohitsangwan@gmail.com>
2022-01-05 18:37:03 +0530

    update for new OS

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+
 export const protect = asyncHandler(async (req, res, next) => {
   let token;
   if (
